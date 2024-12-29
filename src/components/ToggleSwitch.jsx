8fc67670ce057b293b792705d22aa9480ea8a021
DivakarSahu1 <Diwakarsahu2872@gmail.com>
2024-12-29 15:43:15 +0530

    npm icon is added

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { IoIosSwitch } from "react-icons/io";
 import "./ToggleSwitch.css";
 export const ToggleSwitch = () => {
   const [isOn, setIsOn] = useState(false);
@@ -9,14 +10,20 @@ export const ToggleSwitch = () => {
   const CheckIsOn = isOn ? "on" : "off";
   const toggleBgcolor = { backgroundColor: isOn ? "#4caf50" : "#f44336" };
   return (
-    <div
-      className="toggle-switch"
-      style={toggleBgcolor}
-      onClick={handleToggleSwitch}
-    >
-      <div className={`switch ${CheckIsOn}`}>
-        <span className="switch-state">{CheckIsOn}</span>
+    <>
+      <h1 style={{ color: "#000", textAlign: "centre" }}>
+        Toggle Switch
+        <IoIosSwitch style={{ color: "red", textAlign: "centre" }} />
+      </h1>
+      <div
+        className="toggle-switch"
+        style={toggleBgcolor}
+        onClick={handleToggleSwitch}
+      >
+        <div className={`switch ${CheckIsOn}`}>
+          <span className="switch-state">{CheckIsOn}</span>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
